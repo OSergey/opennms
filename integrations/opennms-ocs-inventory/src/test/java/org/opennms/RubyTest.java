392ae04ebd713eb1713591e54e02b9350ded8188
Sergey Ovsyuk <sergey.ovsyuk@gmail.com>
2013-08-28 10:33:20 +0300

    add ruby test

--- integrations/opennms-ocs-inventory/src/test/java/org/opennms/RubyTest.java
+++ integrations/opennms-ocs-inventory/src/test/java/org/opennms/RubyTest.java
@@ -13,7 +13,7 @@ import org.opennms.ocs.inventory.client.response.Computer;
 /**
  * @author <A HREF="mailto:sergey.ovsyuk@gmail.com">Sergey Ovsyuk </A>
  */
-public class ScriptTest {
+public class RubyTest {
     @Test
     public void test() throws IllegalAccessException, IOException, InstantiationException {
     	ManagerScript m_gr = new ManagerScript();
@@ -23,16 +23,17 @@ public class ScriptTest {
         bs.setSManufacturer("Manufacture");
         computer.setBios(bs);
         StringBuilder builder = new StringBuilder();
-        builder.append("import org.opennms.netmgt.provision.persist.requisition.RequisitionNode \n");
-        builder.append("import org.opennms.netmgt.provision.persist.requisition.RequisitionAsset \n");
-        builder.append("RequisitionNode reqNode = new RequisitionNode() \n");
-        builder.append("if (computer.bios != null) { \n");
-        builder.append("reqNode.putAsset(new RequisitionAsset(\"BiosModel\", computer.bios.SModel)) \n");
-        builder.append("}\n return reqNode");
+        builder.append("java_import \"org.opennms.netmgt.provision.persist.requisition.RequisitionNode\" \n");
+        builder.append("java_import \"org.opennms.netmgt.provision.persist.requisition.RequisitionAsset\" \n");
+        builder.append("reqNode = RequisitionNode.new() \n");
+        builder.append("unless $computer.bios.nil? \n");
+        builder.append("reqNode.putAsset(RequisitionAsset.new(\"BiosModel\", $computer.bios.sModel)) \n");
+        builder.append("end \n return reqNode");
 
-        RequisitionNode reqNode = (RequisitionNode)m_gr.executeScript("groovy", builder.toString(), "", computer);
+        RequisitionNode reqNode = (RequisitionNode)m_gr.executeScript("ruby", builder.toString(), "", computer);
         assertNotNull(reqNode);
         System.out.println(reqNode);
+        System.out.println(reqNode.getAssets());
     }
 
 
